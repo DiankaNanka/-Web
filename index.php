519aafd5731adb479ad79d5fe0443e1cc20f1515
DianaNanka <diana.dorokhova@bk.ru>
2020-07-14 16:48:25 +0300

    1

---
+++ index.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="utf-8">
+<title>Udacity</title>
+<style type="text/css">
+   .block1 { 
+    position: relative;
+    bottom: 850px;
+    left: 500px;
+    width: 700px; 
+    height: 90px;
+    background-color: white;
+    padding: 30px;
+    padding-right: 70px; 
+    border: solid 1px white;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0,0,0,0.5);
+   }
+   .block2 {
+      left: 550px;
+      width: 170px; 
+      height: 35px;
+      background-color: #00bfff;
+      border: solid 1px #00bfff;
+      border-radius: 6px;
+      color: white;
+      text-align: center;
+      position: relative;
+      top: -70px;
+   }
+   .block3 {
+      left: 550px;
+      width: 170px; 
+      height: 35px;
+      background-color: white;
+      border: solid 1px #00bfff;
+      border-radius: 6px;
+      color: #00bfff;
+      text-align: center;
+      position: relative;
+      right: -570px;
+      top: -70px;
+   }
+   .block4 {
+    position: relative;
+    bottom: 850px;
+    left: 500px;
+    width: 700px; 
+    height: 45px;
+    background-color: white;
+    padding: 30px;
+    padding-right: 70px; 
+    border: solid 1px white;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0,0,0,0.5);
+   }
+   .block5 {
+      left: 550px;
+      width: 170px; 
+      height: 35px;
+      background-color: white;
+      border: solid 1px #00bfff;
+      border-radius: 6px;
+      color: #00bfff;
+      text-align: center;
+      position: relative;
+      right: -570px;
+      top: -55px;
+   }
+   .color {
+      color: #00bfff;
+   }
+   .color2 {
+      color: white;
+   }
+   .block6 {
+    position: relative;
+    left: 200px;
+    width: 1300px; 
+    height: 0.5px;
+    background-color: white;
+    padding: 30px;
+    padding-right: 70px; 
+    border: solid 1px white;
+    box-shadow: 0 0 10px rgba(0,0,0,0.5);
+   }
+   .block7 {
+    position: relative;
+    bottom: 850px;
+    left: 500px;
+    width: 700px; 
+    height: 90px;
+    background-color: #f4fcfe;
+    padding: 30px;
+    padding-right: 70px; 
+    border: solid 1px white;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0,0,0,0.5);
+   }
+   .block8 {
+    position: relative;
+    bottom: 200px;
+    width: 100px; 
+    height: 800px;
+    background-color: #1f2932;
+    padding: 30px;
+    padding-right: 70px; 
+    border: solid 1px white;
+   }
+   .block9 {
+    position: relative;
+    bottom: 850px;
+    left: 500px;
+   }
+   i {
+    border: solid white;
+    border-width: 0 3px 3px 0;
+    display: inline-block;
+    padding: 3px;
+    position: relative;
+    top: 5px;
+   }
+.right {
+    transform: rotate(-45deg);
+    -webkit-transform: rotate(-45deg);
+   }
+   body {
+      font-family: sans-serif;
+      font-size: 11pt;
+   }
+</style>
+</head>
+
+<body>
+<header>
+<div class="block6"><p>Home</p><span
+style="vertical-align: -6px;" ></div>
+<div style="text-indent:500px">
+<p class="color">COURSES<p><br>
+<p>LATEST ACTIVITY</p>
+</div>
+</header>
+<main>
+<div class="block8">
+<h1 class="color2">UDACITY</h1>
+<img src="" width="25" height="25"><font color="white">Home</font><br><br>
+<font color="white">Catalog</font>
+</div>
+<div class="block7">COURSE<br><br>
+<b>Responsive Web Design Fundamentals</b><br><br>
+Learn and practice strategies for developing websites that look great on every<br>
+device! Made with Google's Pete LePage.
+<div class="block2">COUNTINUE <span style="vertical-align: 6px;" >
+<i class="arrow right" ></i>
+</div>
+</div><br>
+<div class="block9">
+<p>CURRENT COURSE ENROLLMENTS</p>
+</div>
+<div class="block1">COURSE<br><br>
+<b>Responsive Web Design Fundamentals</b><br><br>
+Learn and practice strategies for developing websites that look great on every<br>
+device! Made with Google's Pete LePage.
+<div class="block3">PROGRAM HOME <span style="vertical-align: 6px;" >
+<i class="arrow right"></i>
+</div>
+</div><br>
+<div class="block4">COURSE<br><br>
+<b>Intro to HTML and CSS</b>
+<div class="block5">PROGRAM HOME <span
+style="vertical-align: 6px;" >
+</div>
+</div><br>
+<div class="block1">COURSE<br><br>
+<b>Software Debugging</b><br><br>
+In this course you will learn how to debug programs systematically using.<br>
+scientific methods and build several automated debugging tools in Python.
+<div class="block3">PROGRAM HOME <span
+style="vertical-align: 6px;" >
+</div>
+</div><br>
+<div class="block1">COURSE<br><br>
+<b>Rapid Prototyping</b><br><br>
+Learning to prototype will save you time and money in the development process.<br>
+You'll create quality apps faster and have confidence in the viability of your products.
+<div class="block3">PROGRAM HOME <span
+style="vertical-align: 6px;" >
+</div>
+</div><br>
+<div class="block1">COURSE<br><br>
+<b>Intro to iOS App Development with Swift</b><br><br>
+Take the first step in becoming an iOS Developer by learning Swift and writing<br>
+your first app.
+<div class="block3">PROGRAM HOME <span
+style="vertical-align: 6px;" >
+</div>
+</div><br>
+<div class="block1">COURSE<br><br>
+<b>Intro to HTML and CSS</b><br><br>
+Learn how to convert digital design mockups into static web pages and how to build<br>
+a responsive portfolio site to showcase your work.
+<div class="block3">PROGRAM HOME <span
+style="vertical-align: 6px;" >
+</div>
+</div><br>
+<div class="block1">COURSE<br><br>
+<b>Intro to Algorithms</b><br><br>
+This class will give you an introduction to the design and analysis of algorithms, enabling<br>
+you to analyze networks and discover how individuals are connected.
+<div class="block3">PROGRAM HOME <span
+style="vertical-align: 6px;" >
+</div>
+</div><br>
+</main>
+<footer>
+<div class="block9">
+<p>Help&FAQ   Report An Issue</p>
+</div>
+</footer>
+</body>
